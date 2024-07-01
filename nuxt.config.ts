2b6d99d4ab6cec6352bdb87fe92cf925c33e7bd2
qilip <qilip@qilip.io>
2024-07-01 17:55:12 +0900

    🍎 Chore: husky test disabled
    #

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
   future: {
     compatibilityVersion: 4, // Nuxt 4 대비해서 미리 활성화
+  },
+
+  modules: ['@nuxt/eslint'],
+  eslint: {
+    config: {
+      stylistic: true,
+    },
   },
 })
