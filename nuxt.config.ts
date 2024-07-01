9769cb2c62754f17062f9c3b688b04bd5e714a31
qilip <qilip@qilip.io>
2024-07-01 17:20:18 +0900

    feat: nuxt init

---
+++ nuxt.config.ts
@@ -0,0 +1,7 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  future: {
+    compatibilityVersion: 4, // Nuxt 4 대비해서 미리 활성화
+  },
+})
