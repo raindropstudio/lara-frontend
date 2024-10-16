b20145a955968abfc244ff8b8f99a6b31b6dfa2f
SangMin Shin <qilip@qilip.io>
2024-10-16 09:31:41 +0900

    fix: 타이핑 라이브러리 cjs 오류수정

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -28,6 +28,10 @@ export default defineNuxtConfig({
     dirs: ['types/*.ts'],
   },
 
+  build: {
+    transpile: ['hangul-typing-animation'],
+  },
+
   eslint: {
     config: {
       stylistic: true,
